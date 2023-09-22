562306d80c6d4445357cb55fdcc2e0026092f5c6
focal <zoinexion@gmail.com>
2023-09-22 19:28:11 +0700

    fix bcrypt UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -22,7 +22,7 @@ class UserSeeder extends Seeder
            [
             'name' => "Admin",
             'email' => 'admin@admin.com',
-            'password' => Hash::make('12345678'),
+            'password' => bcrypt('12345678'),
             'is_admin' => true,
             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
             'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
@@ -30,7 +30,7 @@ class UserSeeder extends Seeder
            [
             'name' => "User",
             'email' => 'user@user.com',
-            'password' => Hash::make('12345678'),
+            'password' => bcrypt('12345678'),
             'is_admin' => false,
             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
             'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
