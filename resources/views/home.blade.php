765fee37fd7266623eebd763abd8d378fe8acf99
kuanvell@gmail.com <kuanvell@gmail.com>
2023-09-26 04:55:55 +0700

    fix responsive page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,19 +5,15 @@
 @section('content')
 
 <!-- https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_slideshow_auto -->
-<style>
-
-
-</style>
     {{-- Carousel Start --}}
     <div id="carousel" style="margin-bottom: 8%">
         @foreach ($promos as $promo)
         <div class="slideHomePromo"><a href="#"><img class="homeImage" src="{{Storage::url($promo->photo)}}" alt="fotoimage"></a></div>
         @endforeach
 
-        <div style="position: absolute; margin-left: 45%; margin-top: 1%;">
+        <div class="promoButton" style="position: absolute; margin-left: 45%; margin-top: 1%;">
             <button id="prev" style="float: left; font-size:50%; background-color: rgba(255, 255, 255, 0)"><div class="homeArrow" style="border: 1px solid white; border-radius: 100%; padding: 5%"><i class="fa fa-arrow-left" style="font-size: 150%"></i></div></button>
-            <a class="homeSeeAllPromo" href="http://localhost:8000/promo">See All Promo</a>
+                <a class="homeSeeAllPromo" href="http://localhost:8000/promo">See All Promo</a>
             <button id="next" style="float: left; font-size:50%; background-color: rgba(255, 255, 255, 0)"><div class="homeArrow" style="border: 1px solid white; border-radius: 100%; padding: 5%"><i class="fa fa-arrow-right" style="font-size: 150%"></i></div></button>
         </div>
     </div>
@@ -89,9 +85,9 @@
 
     <!-- Visit Destination Start AKA EXPLORE THE THOUSAND ISLAND -->
     <div>
-        <div style="text-align: center">
+        <div class="subTitleHome" style="text-align: center">
             <h1 style="color: #12AD2B; font-weight:bold">EXPLORE THE THOUSAND ISLAND</h1>
-            <h3 style="color: #EB5406; font-weight:bold">Let's Travel with Rumah Lestari Hijau</h3>
+            <h3 style="color: #EB5406; font-weight:bold; margin-bottom:2%;">Let's Travel with Rumah Lestari Hijau</h3>
             <div style="width: 100%; background-image: url('gambar/autumn-reflections.png'); padding:5%; background-size: cover; ">
               {{-- GAMBAR DIDALAM GAMBAR --}}
                 <img src="gambar/home1.svg" style="width: 25%; margin: 2%">
@@ -99,7 +95,7 @@
                 <img src="gambar/home3.svg" style="width: 25%; margin: 2%">
             </div>
             <a href="/destination"><button style="border-radius: 20px; background: linear-gradient(to right, orange, red); color: white; padding: 1%; padding-left: 3%; padding-right: 3%; translate: 0% -50%; cursor: pointer;">See More</button></a>
-            <p class="paja" style="border-radius: 20px; color: white; translate: 0% -400%; font-size: 300%; font-weight:bold">A Truly EXtraordinary Experience</p>
+            <p class="paja">A Truly EXtraordinary Experience</p>
             {{-- Style= "Translate: X Y" --}}
         </div>
     </div>
@@ -116,7 +112,7 @@
       <div class="container">
         <div class="row">
           <div class="col-6">
-            <h4 class="subTittle">
+            <h4 class="serviceSubTittle">
                 Choose The Place Where you want to Visit
             </h4>
               <div class="imgContainer">
@@ -127,7 +123,7 @@
           </div>
 
           <div class="col-6">
-            <h4 class="subTittle">
+            <h4 class="serviceSubTittle">
                 Explore a Different Way Culinary Experience
             </h4>
               <div class="imgContainer">
--- resources/views/promo.blade.php
+++ resources/views/promo.blade.php
@@ -10,14 +10,13 @@
 
 
   {{-- Tulisan Besar Promo --}}
-  <div style="text-align: center; font-size: 250%; font-weight: bold; margin-top: 10%; margin-bottom: 5%; color:#3bb143">Check Our <b style="color: #FF6700">Promos</b> To Get Special<br>Accommondation Prices Here</div>
+  <div class="promoTitle">Check Our <b style="color: #FF6700">Promos</b> To Get Special<br>Accommondation Prices Here</div>
 
   {{-- What's NEW! --}}
-  <div style="font-size: 250%; margin-left: 5%; color: #3bb143; font-weight: bold; margin-bottom: 2%">What's <b style="color: #006400">NEW!</b></div>
+  <div class="promoSubTittle-2">What's <b style="color: #006400">NEW!</b></div>
 
-  <div id="carousel">
+  <div id="carouselPromo">
       {{---------------- LOOPING WHAT'S NEW LIST ----------------}}
-      {{-- <div class="slidePromo"><a href="#"><img src="gambar/contohgambar.png" class="promoImage"></a></div> --}}
       {{---------------- LOOPING WHAT'S NEW LIST END ----------------}}
 
       @php
@@ -26,16 +25,13 @@
       @endphp
 
       @foreach ($cre as $data)
-      <div class="slidePromo"><a href="#"><img src="{{Storage::url($data->photo)}}" style="height:220px; width:400px" class="promoImage"></a></div>
+      <div class="slidePromo"><a href="#"><img src="{{Storage::url($data->photo)}}" class="promoImage"></a></div>
       @endforeach
-      {{-- <div class="slidePromo"><a href="#"><img src="gambar/contohgambar.png" class="promoImage"></a></div>
-      <div class="slidePromo"><a href="#"><img src="gambar/contohgambar.png" class="promoImage"></a></div>
-      <div class="slidePromo" style="width: 0%"></div> --}}
   </div>
   {{-- What's NEW! End --}}
 
   {{-- Last Activity! --}}
-  <div style="font-size: 250%; margin-left: 5%; color: #3bb143; font-weight: bold; margin-bottom: 2%; margin-top: 2%;">Last<b style="color: #006400"> Activity!</b></div>
+  <div class="promoSubTittle-2">Last<b style="color: #006400"> Activity!</b></div>
   <div id="carousel2">
     {{---------------- LOOPING LAST ACTIVITY LIST ----------------}}
 
@@ -47,7 +43,7 @@
     @endphp
 
     @foreach ($upd as $data)
-    <div class="slidePromo"><a href="#"><img src="{{Storage::url($data->photo)}}" style="height:220px; width:400px;" class="promoImage"></a></div>
+    <div class="slidePromo"><a href="#"><img src="{{Storage::url($data->photo)}}" class="promoImage"></a></div>
     @endforeach
   </div>
 
